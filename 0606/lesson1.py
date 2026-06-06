f66d91a631a78373fa4dc4b95539f24c373a895a
Joy <joylin1211@gmail.com>
2026-06-06 11:53:43 +0800

    提交

--- 0606/lesson1.py
+++ 0606/lesson1.py
@@ -19,3 +19,12 @@ with open("考試分數_3年6班.csv", "r", encoding="utf-8") as file:
     reader = csv.DictReader(file)
     for row in reader:
         print(row)
+
+        import csv
+
+with open("考試分數_3年6班.csv", "r", encoding="utf-8") as file:
+    reader = csv.DictReader(file)
+    print(type(reader))
+    for row in reader:
+        if int(row['數學']) > 90:
+            print(row['學生姓名'])
